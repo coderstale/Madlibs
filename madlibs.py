b73fffd1c8cd40ea3b0702ec4a803b5f74bc38ee
Tom <85279322+coderstale@users.noreply.github.com>
2022-02-14 11:29:40 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+color = input("Pick a Color: ")
+plural_noun = input("Pick a Plural Noun: ")
+person = input("Name of the person: ")
+madlib = f"Roses are {color} \n{plural_noun} are blue \nI love {person}"
+print(madlib)
